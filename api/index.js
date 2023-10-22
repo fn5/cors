890c31d702e9e6009da167ddb0bebab2192d2476
Giasin <94991581+Giasinit@users.noreply.github.com>
2023-10-22 16:23:57 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,49 +1,43 @@
 import express from 'express';
-import cors from 'cors';
-import request from 'request';
+import cors    from 'cors';
+import request from 'request'
 
-function parseProxyParameters(proxyRequest) {
-  const params = {};
-  // Check if there is an URL directly in the path
-  const urlMatch = proxyRequest.url.match(/^\/(https?:\/\/.+)/);
-  if (urlMatch) {
-    params.url = decodeURIComponent(urlMatch[1]);
-  } else {
-    // If not in the path, check for the url query parameter
-    const queryMatch = proxyRequest.url.match(/(?<=[?&])url=(?<url>.*)$/);
-    if (queryMatch) {
-      params.url = decodeURIComponent(queryMatch.groups.url);
-    }
+function parseProxyParameters(proxyRequest){
+  const params = {}
+  // url - treat everything right to url= query parameter as target url value
+  const urlMatch = proxyRequest.url.match(/(?<=[?&])url=(?<url>.*)$/)
+  if(urlMatch) {
+    params.url =  decodeURIComponent(urlMatch.groups.url)
   }
-
-  return params;
+  
+  return params
 }
 
 const app = express();
 app.use(cors());
-app.set('json spaces', 2);
-
+app.set('json spaces', 2)
 app.all('/*', async (req, res) => {
   try {
-    const proxyParams = parseProxyParameters(req);
-    if (!proxyParams.url) {
+    const proxyParams = parseProxyParameters(req)
+    if(!proxyParams.url) {
       return res.status(400).json({
-        title: 'CORS Proxy Error - Required parameter is missing',
-        detail: 'The parameter: url was not provided',
-      });
+        "title": "CORS Proxy Error - Required parameter is missing",
+        "detail": "The parameter: url was not provided",
+      }) 
     }
-
-    // Proxy request to the target URL
-    const target = request(proxyParams.url);
-    req.pipe(target);
-    target.pipe(res);
-  } catch (err) {
-    console.error(err);
+    
+    // proxy request to target url
+    const target = request(proxyParams.url)
+    req.pipe(target)
+    target.pipe(res)
+    
+  } catch(err) { 
+    console.error(err)
     return res.status(500).json({
-      title: 'CORS Proxy Error - Internal server error',
-      detail: err.message,
-    });
+      "title": "CORS Proxy Error - Internal server error",
+      "detail": err.message,
+    }) 
   }
-});
+})
 
 export default app;
